6e4df75f787390f7eb53cc26104d813df44ceb68
krishnagupta110094 <krishnagupta110094@gmail.com>
2025-08-02 18:43:08 +0530

    reverse

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import ShopContextProvider from "./context/ShopContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/Forever/">
+  <BrowserRouter>
     <ShopContextProvider>
       <App />
     </ShopContextProvider>
